85331144e56f8f1467269ec26d2d82c0e39f50e2
qinhai_Jack <1198617472@qq.com>
2022-05-10 18:07:39 +0800

    add gitignore

---
+++ pages/user/user.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		<button @click="login">LOGIN</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			login(){
+				uni.login({
+					success(res) {
+						console.log(res,'res')
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
