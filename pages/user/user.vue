5cfe22958d45002f939817b0cc25fe87df849835
qinhai_Jack <1198617472@qq.com>
2022-05-10 19:06:11 +0800

    19.00

--- pages/user/user.vue
+++ pages/user/user.vue
@@ -13,11 +13,24 @@
 		},
 		methods: {
 			login(){
-				uni.login({
-					success(res) {
-						console.log(res,'res')
+				uni.getProvider({
+					service: 'oauth',
+					success: function (res) {
+						console.log(res.provider)
+						uni.login({
+							provider: res.provider[0],
+							scopes:'auth_user',
+							success({code}) {
+								uni.getUserInfo({
+									provider: res.provider[0],
+									success(user){
+										console.log(user,'user')
+									},
+								})
+							}
+						})
 					}
-				})
+				});
 			}
 		}
 	}
