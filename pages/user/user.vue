742e04262c7814ef31152d83e5304f0087275590
qinhai_Jack <1198617472@qq.com>
2022-05-11 18:27:40 +0800

    ad

--- pages/user/user.vue
+++ pages/user/user.vue
@@ -1,57 +1,59 @@
 <template>
 	<view>
 		<!-- #ifdef MP-WEIXIN -->
-		<button open-type="getUserInfo" @getuserinfo="getuserinfo">LOGIN</button>
-		<!-- #endif -->
-		<!-- #ifdef MP-ALIPAY -->
-		<button 
-			open-type="getAuthorize" 
-			scope="userInfo"
-			onGetAuthorize="onGetAuthorize"
-			@onGetAuthorize="getuserinfo"
-			onError="onAuthError"
-		>
-			ALI-LOGIN
-		</button>
+		<view @click="login">LOGIN</view>
 		<!-- #endif -->
 	</view>
 </template>
 <script>
-	export default {
-		data() {
-			return {
-				
-			}
+const db = uniCloud.database()
+export default {
+	data() {
+		return {};
+	},
+	methods: {
+		login(e) {
+			// #ifdef MP-WEIXIN
+			uni.getUserProfile({
+				desc: '为了更好的服务',
+				success(userinfo) {
+					console.log(userinfo, 'userinfo');
+				},
+				fail(err) {
+					console.error(err, 'err');
+				}
+			});
+			//#endif
 		},
-		methods: {
-			getuserinfo(e){
-				console.log(e, 'e')
-			},
-			onGetAuthorize(e) {
-				console.log(e, 'onGetAuthorize')
-				// #ifdef MP-ALIPAY
-				
-					my.getOpenUserInfo({
-						fail: (res) => {
-							console.log(res, 'fail res')
+		getUserInfo() {
+			my.getAuthCode({
+				scopes: 'auth_user',
+				fail: error => {
+					console.error('getAuthCode', error);
+				},
+				success: () => {
+					// do login...
+					// then
+					my.getAuthUserInfo({
+						fail: error => {
+							console.error('getAuthUserInfo', error);
 						},
-						success: (res) => {
-							console.log(res, 'success res')
-							let userInfo = JSON.parse(res.response).response // 以下方的报文格式解析两层 response
+						success: userInfo => {
+							console.log(`userInfo:`, userInfo);
+							this.setData({
+								userInfo,
+								hasUserInfo: true
+							});
+							abridge.alert({
+								title: JSON.stringify(userInfo) // alert 框的标题
+							});
 						}
 					});
-				// #endif
-			},
-			onAuthError(err) {
-				console.log(err, 'err')
-			},
-			onClick(e) {
-				console.log(e, 'onclick')
-			}
+				}
+			});
 		}
 	}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
